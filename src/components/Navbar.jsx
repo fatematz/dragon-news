de81c7197c9f4c2ddd8d2ac37fe2ba1a281b009d
Fatema <fatematz486@gmail.com>
2026-04-25 10:16:15 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,31 @@
+'use client'
+import Link from "next/link";
+import userAvatar from '@/assets/user.png'
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
-const Navbar = () => {
+const Navbar=() => {
+    
+        const pathname = usePathname();
+
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className="mt-[30px]">
+        <div className="flex justify-between items-center container ">
+            <div className="">
+                
+            </div>
+            <div className="">
+                <ul className="flex justify-center gap-5 ">
+                    <li> <Link href='/' className={pathname === '/' ? 'text-red-500 font-bold border-b-2 border-red-500' : ''}  >Home</Link> </li>
+                    <li> <Link href='/about' className={pathname === '/' ? 'text-red-500 font-bold border-b-2 border-red-500' : ''} >About</Link> </li>
+                    <li> <Link href='/career' className={pathname === '/' ? 'text-red-500 font-bold border-b-2 border-red-500' : ''} >Career</Link> </li>
+                </ul>
+            </div>
+            <div className="flex justify-center gap-4">
+                <Image src={userAvatar} alt="avatar img" />
+                <Link className="btn bg-gray-600 text-white" href="/login">Login</Link>
+            </div>
+        </div>
         </div>
     );
 };
