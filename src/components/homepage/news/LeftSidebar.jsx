de08ffb24fd740981f787c8dee981593c9d8c5a9
Anisur Rahman <fatematz486@gmail.com>
2026-04-25 19:27:59 +0600

    Update LeftSidebar.jsx

--- src/components/homepage/news/LeftSidebar.jsx
+++ src/components/homepage/news/LeftSidebar.jsx
@@ -10,7 +10,7 @@ const LeftSideBar = ({categories, activeId}) => {
     <ul className="flex flex-col gap-[20px]">
     {
       categories.news_category.map(category => {
-        return <li className={` ${activeId === category.category_id && "border-2 border-b-pink-500"}  py-3  ml-5`}key={category.category_id}> {category.category_name} </li>
+        return <li className={` pl-6 ${activeId === category.category_id && "rounded-2xl border-2 border-b-pink-500"}  py-3  `}key={category.category_id}> {category.category_name} </li>
       })
     }
     </ul >
