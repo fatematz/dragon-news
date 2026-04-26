4d1b61f9e1ddb063f1c6a810045e269233ac71ab
Anisur Rahman <fatematz486@gmail.com>
2026-04-26 11:46:29 +0600

    update

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -8,24 +8,51 @@ async function getCategories(){
   return data.data;
 }
 
+
+  async function getNewsCategoryId( category_id ){
+  const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
+  const data = await res.json();
+  return data.data;
+}
+
+
+
 export default async function Home() {
   const categories = await getCategories();
   console.log(categories.news_category, "category");
 
-  return (
-    <div className="container grid grid-cols-12 gap-4 ">
+  const news = await getNewsCategoryId("05")
+  console.log(news)
 
+  return (
+    <>
+    {/* first  */}
+    <div className="mt-16">
+    <div className="container grid grid-cols-12 gap-4 my-4">
+      
       <div className=" col-span-3">
        <LeftSideBar categories={categories} activeId={"04"}/>
       </div>
 
       <div className="bg-gray-500 col-span-6">
-      <h1>center</h1>
+      <h1>All News</h1>
+      {
+        news.map(n => {
+          return <div key={n._id}> {n.title} </div>
+        })
+      }
       </div>
 
-      <div className="bg-green-300 col-span-3">
+      <div className=" col-span-3">
       <RightSideBar/>
       </div>
       </div>
+      </div>
+
+    
+
+
+</>
+      
   );
 }
