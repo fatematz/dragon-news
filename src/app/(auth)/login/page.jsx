1316639060425000e404d212ba6df0356c8652c1
Anisur Rahman <fatematz486@gmail.com>
2026-04-26 17:14:42 +0600

    update login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -6,9 +6,11 @@ const LoginPage = () => {
 
      const {
     register,
-    handleSubmit
+    handleSubmit, 
+    formState : {errors},
      } = useForm()
 
+     console.log(errors, "errors")
 
     const handleLoginFun = (data) => {
 
@@ -20,21 +22,22 @@ const LoginPage = () => {
         // console.log(email, password)
     }
     return (
-        <div className=" min-h-[80vh] bg-slate-100 pt-16 ">
+        <div className=" min-h-[100vh] bg-slate-100 pt-16 mt-6 ">
             <div className="rounded-xl p-4 bg-white max-w-[752px] mx-auto">
             <h1 className="text-center font-bold text-3xl border-b py-8 mx-15">Login Your Account</h1>
 
             <form className="ml-52 my-6" onSubmit={handleSubmit (handleLoginFun)}>
                  <fieldset className="fieldset">
   <legend className=" font-semibold text-[20px] fieldset-legend">Email address</legend>
-  <input type="email" {...register("email", {required: true} ) } className="input" placeholder="Enter your email address" />
+  <input type="email" {...register("email", {required: "email field is required"} ) } className="input" placeholder="Enter your email address" />
 
+{errors.email && <p className="text-red-500"> {errors.email.message} </p>}
 </fieldset>
 
  <fieldset className="fieldset">
   <legend className="font-semibold text-[20px] fieldset-legend">Password</legend>
-  <input type="password" {...register("password" , {required: true})} className="input" placeholder="Enter your password" />
-
+  <input type="password" {...register("password" , {required: "password field is required" })} className="input" placeholder="Enter your password" />
+{errors.password && <p className="text-red-500"> {errors.password.message} </p>}
 </fieldset>
 
 <button className="btn bg-[#403F3F] text-white w-[320px] mt-[20px]">Login</button>
