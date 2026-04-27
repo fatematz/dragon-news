11f25ec3d66a322b683ec1bfad872874d0961352
Fatema <fatematz486@gmail.com>
2026-04-27 14:44:01 +0600

    Create auth.js

---
+++ src/lib/auth.js
@@ -0,0 +1,19 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import dns from "node:dns/promises";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+   
+    client
+  }),
+
+   emailAndPassword: { 
+    enabled: true, 
+  },
+});
