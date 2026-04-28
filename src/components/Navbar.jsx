691afc966d6cabbec7cfef88ba16a1a92453d888
Fatema <fatematz486@gmail.com>
2026-04-28 11:25:57 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,28 @@
 import Link from "next/link";
 import userAvatar from '@/assets/user.png'
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter  } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
 
 const Navbar=() => {
+
+    const { data: session } = authClient.useSession()
+
+    console.log(session, "session");
+    
     
         const pathname = usePathname();
+            const router = useRouter();
+
+              const handleSignOut = async () => {
+        await authClient.signOut({
+            fetchOptions: {
+                onSuccess: () => {
+                    router.push("/login");
+                }
+            }
+        });
+    };
 
     return (
         <div className="mt-[30px] mt-[30px]">
@@ -21,11 +38,26 @@ const Navbar=() => {
                     <li> <Link href='/career' className={pathname === '/career' ? 'text-red-500 font-bold border-b-2 border-red-500' : ''} >Career</Link> </li>
                 </ul>
             </div>
-            <div className="flex justify-center gap-4">
-                <Image src={userAvatar} alt="avatar img" />
-                <Link  className="btn bg-gray-600 text-white" href="/login">Login</Link>
-                <Link className="btn bg-gray-600 text-white" href="/register">register</Link>
-            </div>
+
+            <div className="flex justify-center items-center gap-4">
+  <Image src={session?.user?.image || userAvatar} alt="avatar img" width={40} height={40} className="rounded-full " />
+  
+  {session ? (
+
+    <>
+      <span> Hello {session.user.name}</span>
+      <button className="btn bg-gray-600 text-white" onClick={handleSignOut}>Logout</button>
+    </>
+  ) : (
+
+
+
+    <>
+      <Link className="btn bg-gray-600 text-white" href="/login">Login</Link>
+      <Link className="btn bg-gray-600 text-white" href="/register">Register</Link>
+    </>
+      )}
+</div>
         </div>
         </div>
     );
