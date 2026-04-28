4981830489b2ee27cc3d72500673a127f44a966b
Fatema <fatematz486@gmail.com>
2026-04-28 16:06:58 +0600

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -16,4 +16,21 @@ export const auth = betterAuth({
    emailAndPassword: { 
     enabled: true, 
   },
+
+   socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
+    },
+
+    user: {
+    additionalFields: {
+      image: {
+        type: "string",
+        required: false,
+      }
+    }
+  }
+
 });
